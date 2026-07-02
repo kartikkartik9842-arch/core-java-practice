d2001ce1086d7691fe06c280990252e7e7d0b500
Kartik <kartik.kartik@conduent.com>
2026-07-02 19:58:13 +0530

    Add inheritance examples

---
+++ src/Oops/Inheritance/Dog.java
@@ -0,0 +1,15 @@
+package Oops.Inheritance;
+
+public class Dog extends Animal {
+
+    String breed;
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public void bark() {
+        System.out.println(name + " is barking.");
+    }
+}
